a319b32a54fa85d7b7164584d24129e512f95571
Kübra Moğul <kubra.mogul.95@gmail.com>
2020-12-13 20:42:06 +0300

    ilk gönderi

---
+++ MyBlog_Admin/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlog_Admin.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
